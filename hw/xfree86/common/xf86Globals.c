78318722207bb7d8bc0e111e92766e20f0442fcc
Kristian Høgsberg <krh@redhat.com>
2009-09-22 21:19:47 -0400

    Add flag to toggle rootless

--- hw/xfree86/common/xf86Globals.c
+++ hw/xfree86/common/xf86Globals.c
@@ -197,3 +197,5 @@ Bool xf86VidModeAllowNonLocal = FALSE;
 #endif
 RootWinPropPtr *xf86RegisteredPropertiesTable = NULL;
 Bool xorgHWAccess = FALSE;
+Bool xorgWayland = FALSE;
+Bool xorgRootless = FALSE;
