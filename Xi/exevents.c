faf7f307f1d0fd93d0f91fe9fba1c5ca888c2f5e
Kristian Høgsberg <krh@bitplanet.net>
2011-02-07 20:27:47 -0500

    hosted: Track Wayland API changes, add cursor forwarding

--- Xi/exevents.c
+++ Xi/exevents.c
@@ -2007,7 +2007,7 @@ SetDeviceRedirectWindow(DeviceIntPtr dev, WindowPtr window)
     SpritePtr pSprite = dev->spriteInfo->sprite;
 
     pSprite->redirectWindow = window;
-    CheckMotion(NULL, dev->u.master);
+    CheckMotion(NULL, dev);
 }
 
 int
