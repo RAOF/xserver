761bf7584c522df1fd7361a4dd78f25ecfb71adf
Corentin Chary <corentin.chary@gmail.com>
2011-06-06 16:36:28 +0200

    s/hosted/xwayland/
    Signed-off-by: Corentin Chary <corentin.chary@gmail.com>

--- hw/xfree86/common/xf86Globals.c
+++ hw/xfree86/common/xf86Globals.c
@@ -197,5 +197,5 @@ Bool xf86VidModeAllowNonLocal = FALSE;
 #endif
 RootWinPropPtr *xf86RegisteredPropertiesTable = NULL;
 Bool xorgHWAccess = FALSE;
-Bool xorgHosted = FALSE;
+Bool xorgWayland = FALSE;
 Bool xorgRootless = FALSE;
