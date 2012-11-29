f516710e75e1f6d2a6b517ddab59ffda89bec75e
Christopher James Halse Rogers <raof@ubuntu.com>
2012-11-29 20:10:54 +1100

    More more more

--- hw/xfree86/xmir/xmir.h
+++ hw/xfree86/xmir/xmir.h
@@ -7,14 +7,27 @@
 
 #include <mir_client_library.h>
 
+typedef void (*xmir_buffer_available_callback)(WindowPtr win, void *ctx);
+typedef struct xmir_screen xmir_screen;
+
+typedef struct xmir_buffer_info {
+    uint32_t name;
+    uint32_t stride;
+} xmir_buffer_info;
+
 _X_EXPORT int
-xmir_get_drm_fd(void);
+xmir_get_drm_fd(xmir_screen *screen);
+
+_X_EXPORT xmir_screen *
+xmir_screen_create(ScrnInfoPtr scrn);
 
 _X_EXPORT Bool
-xmir_mode_init(ScrnInfoPtr scrn);
+xmir_mode_init(xmir_screen *screen);
 
-_X_EXPORT Bool
-xmir_start_buffer_loop(mir_surface_lifecycle_callback callback, void *ctx);
+_X_EXPORT void
+xmir_populate_buffers_for_window(WindowPtr win, xmir_buffer_info *buf);
 
-_X_EXPORT Bool
-xmir_populate_buffers_for_window(WindowPtr win, MirBufferPackage *bufs);
+_X_EXPORT void
+xmir_submit_rendering_for_window(WindowPtr win,
+                                 RegionPtr region,
+                                 xmir_buffer_available_callback callback);
